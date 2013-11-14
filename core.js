e741af0bf6a5c9650f92295b2b5c3f44a2ca404a
EdwonLim <edwon.lim@gmail.com>
2013-11-15 01:20:40 +0800

    update

--- core.js
+++ core.js
@@ -1,7 +1,60 @@
 (function(module) {
 
-    module.exports = {
+    var Core = {
         init : require('./lib/UserInfo').init,
+        configure : function(options) {
+            if (options) {
+                var process;
+                if (options.floor) {
+                    Core.ReplyManager.openFloor(options.floor);
+                }
+                if (options.forText) {
+                    process = Core.ReplyProcess.replyForText;
+                    process.init(options.forText);
+                    Core.ReplyManager.addProcess(process);
+                }
+                if (options.forEvent) {
+                    process = Core.ReplyProcess.replyForEvent;
+                    process.init(options.forEvent);
+                    Core.ReplyManager.addProcess(process);
+                }
+                if (options.customer) {
+                    process = new Core.ReplyProcess.CustomerService();
+                    process.setTime(options.customer.startTime, options.customer.endTime);
+                    process.setKey(options.customer.key);
+                    process.setWorkers(options.customer.workers);
+                    process.setContent(options.customer.content);
+                    Core.ReplyManager.addProcess(process);
+                }
+                if (options.event) {
+                    options.event.forEach(function(e) {
+                        switch (e.type) {
+                            case 'Lottery' :
+                                process = new Core.ReplyProcess.Lottery();
+                                process.setTime(e.startTime, e.endTime);
+                                process.setKey(e.key);
+                                process.setContent(e.content);
+                                e.prize.forEach(function(item){
+                                    process.addPrize.apply(process, item);
+                                });
+                                process.setLogFile(e.log);
+                                Core.ReplyManager.addProcess(process);
+                                break;
+                            case 'SecKill' :
+                                process = new Core.ReplyProcess.SecKill();
+                                process.setTime(e.startTime, e.endTime);
+                                process.setKey(e.key);
+                                process.setContent(e.content);
+                                process.setPrize(e.prize.name, e.prize.num);
+                                process.setLogFile(e.log);
+                                Core.ReplyManager.addProcess(process);
+                                break;
+                       }
+                    });
+                }
+            }
+            Core.ReplyManager.start();
+        },
         listener : require('./lib/network/MessageListener'),
         send : require('./lib/network/MessageSend'),
         reply : require('./lib/network/MessageReply'),
@@ -17,5 +70,7 @@
         OpenAPI : require('./lib/network/OpenAPI'),
         Debug : require('./lib/util/Debug')
     };
+
+    module.exports = Core;
 
 })(module);
